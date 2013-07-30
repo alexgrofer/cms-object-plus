b2d506e1677ff552872cb4cce55f11ef09864f6d
alexgro <you@example.com>
2013-07-30 23:52:06 +0400

    get model sys new classes

--- myobj/appscms/config/objects.php
+++ myobj/appscms/config/objects.php
@@ -55,10 +55,34 @@ $objects = array(
 			'groups_write' => null,
 		),
 	),
+	'myObjHeaders' => array(
+		'default' => array(
+			'cols' => array('id'=>'id','name'=>'name'), //колонки в списке
+			//названия гуппы outside identifier
+			'groups_read' => null,
+			'groups_write' => null,
+			'order_by_def' => array('id desc'), //сортировка по умолчанию
+		),
+		//headers links example news
+		'news' => array(
+			'cols' => array('id'=>'id','name'=>'name'),
+			'cols_props' => array('text_news'=>'text_news','annotation_news'=>'annotation_news'), //колонки-свойства объектов
+			'find' => array('id', 'text_news__prop'), //разрешенные для поиска
+			'order_by' => array('id','name', 'text_news__prop', 'annotation_news__prop'), //сортирова в меню
+			'order_by_def' => array('annotation_news__prop desc'),
+		),
+		'news_section' => array(
+			'cols' => array('id'=>'id','name'=>'name'),
+			'cols_props' => array('codename_news_section'=>'codename_news_section'),
+		),
+	),
 );
-
-$set_spaces = array(
-	'2' => array('namemodel'=>'systemObjHeaders','namelinksmodel'=>'linksObjectsAllSystem'),
-);
+/*
+ * namemodel -название модели таблицы в которой лежат объекты
+ * namelinksmodel - (модель для ссылок) если в классе будут предусмотренны ссылки на другие объекты,
+ * если null ссылки для объектов этого табличного пространства не предусмотренны
+ */
+$set_spaces['1'] = array('namemodel'=>'myObjHeaders','namelinksmodel'=>'linksObjectsAllMy'); //пользовательские классы
+$set_spaces['2'] = array('namemodel'=>'systemObjHeaders','namelinksmodel'=>'linksObjectsAllSystem'); //системные классы
 require(dirname(__FILE__).'/user/objects.php');
 $objects = array_merge($objects,$objects_user);
