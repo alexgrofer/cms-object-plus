bfd6873134e1b34570d94d84e2ace29f2b2931aa
alexgro <you@example.com>
2013-08-06 00:38:29 +0400

    none files dep

--- myobj/appscms/config/UI.php
+++ myobj/appscms/config/UI.php
@@ -1,9 +1,10 @@
 <?php
+/*
 $ui = array(
-	//Независимые интерфейсы, к примеру вывести график продаж или другой интерфейс. В контроллере можно организовать шаблон, представление и т.д.
-	'graphic_sale' => array(
-		'controller' => array('default'=>'admin/dep_store/graphic_sale.php'),
+	'name' => array(
+		'controller' => array('default'=>'name'),
 		'groups_read' => null,
 		'groups_write' => array('CC99CD08-A1BF-461A-B1FE-3182B24D2812'),
 	),
 );
+*/
--- myobj/controllers/cms/admin/dep_store/graphic_sale.php
+++
@@ -1,2 +0,0 @@
-<?php
-$view = '/cms/views/sys/dep_store/graphic_sale';
