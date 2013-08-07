d843ef02246c2c53a756bbd68358263ebf636e3a
alexgro <you@example.com>
2013-08-08 00:15:40 +0400

    index example content

---
+++ myobj/views/cms/templates/example/index_content.php
@@ -0,0 +1,8 @@
+<table border="1" width="100%">
+	<tr>
+		<td><?php $this->apcms->handle('top menu',1);?></td>
+	</tr>
+	<tr>
+		<td><?php $this->apcms->handle('content',2);?></td>
+	</tr>
+</table>
