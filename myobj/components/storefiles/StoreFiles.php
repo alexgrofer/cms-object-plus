74a38b4026252fc60b4feab435e990ed17134503
alexgro <you@example.com>
2013-08-12 01:57:42 +0400

    storeFiles v3

--- myobj/components/storefiles/StoreFiles.php
+++ myobj/components/storefiles/StoreFiles.php
@@ -9,6 +9,32 @@ class StoreFiles extends CComponent {
 	}
 	public function init() {
 		//import
+		Yii::import('application.modules.myobj.components.storefiles.classes.*');
+	}
+	/**
+	 * Получает массив объектов @link filesStorage с массивом файлов класса CFileStor
+	 * @param array $ids массив ключей для поиска объектов
+	 * @param string $model название модели в которой хранятся файлы
+	 * @return array массив объектов CFilesStor
+	 */
+	public function getobj($ids,$model) {
+
+	}
+	/**
+	 * Добавляет и сохраняет новый файл к объекту
+	 * @param array $ids массив ключей для поиска объектов
+	 * @param string $name название файла
+	 * @param string $title описание файла
+	 * @return CFilesStor объект файла
+	 */
+	public function addobj($ids,$name,$title) {
+
+	}
+	/**
+	 * Удаляет объекты @link CFilesStor, включая сами файлы
+	 * @param array $ids массив ключей для поиска объектов
+	 */
+	public function delobj($ids,$model) {
 
 	}
 }
---
+++ myobj/components/storefiles/classes/CFileStor.php
@@ -0,0 +1,50 @@
+<?php
+class CFileStor {
+	/**
+	 * @var StoreFiles родительский объект который редактируем
+	 */
+	protected $_objStore;
+	/**
+	 * Получить ключ сортировки
+	 * @return integer ключ в списке объекта $_objStore::url
+	 */
+	public function getKey() {
+
+	}
+	/**
+	 * Установить новый ключ сортировки
+	 * @param integer $key ключ для сортировки
+	 */
+	public function setKey($key) {
+
+	}
+	/**
+	 * Получение описание файла
+	 * @return string описание файла
+	 */
+	public function getTitle() {
+
+	}
+	/**
+	 * Задать описание для файла
+	 * @param string $name название описания
+	 */
+	public function setTitle($name) {
+
+	}
+	public function getName() {
+
+	}
+	public function setName() {
+
+	}
+	public function getUrl() {
+
+	}
+	public function save() {
+
+	}
+	public function del() {
+
+	}
+}
