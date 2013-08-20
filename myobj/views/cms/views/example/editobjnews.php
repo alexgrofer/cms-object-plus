6a9f74ac29a3bd9954efe227ed6d38cc50515d7e
alexgro <you@example.com>
2013-08-21 01:21:10 +0400

    example news

--- myobj/views/cms/views/example/editobjnews.php
+++ myobj/views/cms/views/example/editobjnews.php
@@ -6,5 +6,27 @@
  * отсортировать фото
  */
 
+//$model = uClasses::getclass('news_example')->objects()->findByAttributes(); //найти
+$model = uClasses::getclass('news_example')->initobject(); //создать новый
+$addelem = array();
+$addelem[] = array('name'=>'file', 'def_value'=>'dfdf', 'elem'=>array('type'=>'file'));
+$form = $model->UserFormModel->initform($_POST,array('name'=>'name2','file'=>'файлы'),$addelem);
+$form->attributes = array('enctype' => 'multipart/form-data');
 
-?>
+$form->activeForm = array(
+	'enableClientValidation'=>true,
+	'clientOptions'=>array(
+		'validateOnSubmit'=>true,
+	),
+);
+echo $form->renderBegin();
+
+foreach($form->getElements() as $element) {
+	echo $element->render();
+}
+
+
+echo '<p>'.CHtml::submitButton('save').'</p>';
+
+
+echo $form->renderEnd();
