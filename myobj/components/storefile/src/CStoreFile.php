6bd7e5073479d46809c509051089dab220188c82
alexgro <you@example.com>
2013-09-15 02:23:14 +0400

    storefile3

--- myobj/components/storefile/src/CStoreFile.php
+++ myobj/components/storefile/src/CStoreFile.php
@@ -1,18 +1,56 @@
 <?php
 class CStoreFile {
 	/** @var null объект */
-	private $_namePlugin=null;
+	private $_id;
+	private $_arrayNameTitle;
+	private $_namePlugin;
+	public static function init($nameClassPlugin, $objAR) {
+		return new self($nameClassPlugin, $objAR);
+	}
 	public function getNamePlugin() {
 		return $this->_namePlugin;
 	}
+	private function __construct($nameClassPlugin, $objAR) {
+		$this->_namePlugin = $nameClassPlugin;
+	}
 
-	public function add() {
+	/**
+	 * @param $name имя файла
+	 * @param integer $key по умолчанию 0 для нового элемента
+	 */
+	public function name($name,$key=0) {
 
 	}
-	public function edit() {
+
+	/**
+	 * @param $title описание файла
+	 * @param integer $key по умолчанию 0 для нового элемента
+	 */
+	public function title($title,$key=0) {
 
 	}
-	public function del() {
+
+	/**
+	 * @param $path путь к файлу
+	 * @param integer $key по умолчанию 0 для нового элемента
+	 */
+	public function file($path,$key=0) {
+
+	}
+
+	/**
+	 * @param $name имя файла
+	 * Сохранить файл в базе и на сервере (может работать через сокет если это описано в плагине)
+	 */
+	public function save() {
+
+	}
+
+	/**
+	 * Удалить файл и изменить объект
+	 * @param integer $key по умолчанию 0 для нового элемента
+	 */
+	public function del($key) {
 
 	}
 }
