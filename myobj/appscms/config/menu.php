56b63b51de1562cfc841c62a714f6651ca39fb8a
alexgro <you@example.com>
2013-10-05 22:48:07 +0400

    normal menu

--- myobj/appscms/config/menu.php
+++ myobj/appscms/config/menu.php
@@ -1,8 +1,11 @@
 <?php
 //sys
 $menu = array(
-	'classes'=>array('label'=>'classes', 'url'=>array('admin/objects/models/classes')),
-	'properties'=>array('label'=>'properties', 'url'=>array('admin/objects/models/properties')),
+	'classes'=>array('label'=>'classes', 'url'=>array('admin/objects/models/classes'),
+		'items'=>array(
+			'properties'=>array('label'=>'properties', 'url'=>array('admin/objects/models/properties')),
+		),
+	),
 	'mvc'=>array(
 		'label'=>'mvc','items'=>array(
 			'templates'=>array('label'=>'templates','url'=>array('admin/objects/class/templates_sys')),
@@ -11,16 +14,15 @@ $menu = array(
 			'controllers'=>array('label'=>'controllers','url'=>array('admin/objects/class/controllersnav_sys/')),
 		),
 	),
-	'groups'=>array('label'=>'groups', 'url'=>array('admin/objects/class/groups_sys/')),
-	'ui'=>array(
-		'label'=>'UI','items'=>array(
+	'conf'=>array(
+		'label'=>'conf','items'=>array(
+			'groups_sys'=>array('label'=>'groups sys', 'url'=>array('admin/objects/class/groups_sys/')),
 			'user'=>array(
-					'label'=>'user','url'=>array('admin/objects/models/user'),'items'=>array(
-						'group'=>array('label'=>'group','url'=>array('admin/objects/models/group')),
-						'userpasport'=>array('label'=>'userpasport','url'=>array('admin/objects/models/userpasport')),
-					),
+				'label'=>'user','url'=>array('admin/objects/models/user'),'items'=>array(
+					'group'=>array('label'=>'group','url'=>array('admin/objects/models/group')),
+					'userpasport'=>array('label'=>'userpasport','url'=>array('admin/objects/models/userpasport')),
+				),
 			),
-			'storage files'=>array('label'=>'storage files','url'=>array('admin/objects/models/storagef')),
 			'db damps'=>array('label'=>'db damps','url'=>array('admin/objects/class/db_dump_sys','usercontroller'=>'userdbdamp')),
 		),
 	),
