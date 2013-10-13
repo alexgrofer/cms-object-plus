aa2d9208a8fedad9dd7c025978761daece234c4d
alexgro <you@example.com>
2013-10-13 22:12:35 +0400

    sub menu part1

--- myobj/views/layouts/admin/main.php
+++ myobj/views/layouts/admin/main.php
@@ -3,11 +3,6 @@
 <head>
 <meta charset="utf-8" >
 </head>
-<style>
-form .row {padding:0;margin-left:0}
-input, select {height: auto !important; }
-.padding_10 {padding:10px}
-</style>
 <body>
 <div class="padding_10">
 <?php if(!Yii::app()->user->isGuest) { ?>
@@ -16,14 +11,34 @@ $(function() {
     $( "#myMenu" ).menu();
 });
 </script>
+<style>
+.myMenuSub > li {display: none}
+.myMenuSub .active {display: block}
+.myMenuSub .active .active > a {color: red}
+</style>
 <div class="horizontal-menu">
 <?php
-$this->widget('zii.widgets.CMenu',array(
+$objMenu = $this->widget('zii.widgets.CMenu',array(
     'id'=>'myMenu',
     //'htmlOptions'=>array('class'=>'nav'),
     'items'=>yii::app()->appcms->config['controlui']['menu'],
     'activateParents'=>true,
 ));
+$arrSubMenu = array();
+foreach($objMenu->items as $keyElem => $arItem) {
+    if($arItem['active']) {
+        $arrSubMenu = $arItem['items'];
+        break;
+    }
+}
+if($arrSubMenu) {
+    $this->widget('zii.widgets.CMenu',array(
+        'id'=>'myMenuSub',
+        'htmlOptions'=>array('class'=>'myMenuSub'),
+        'items'=>$arrSubMenu,
+        'activateParents'=>true,
+    ));
+}
 ?>
 </div>
 <?php
