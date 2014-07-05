cc9da918e838ad9ea999710ce06a0f02ff84243c
root <root@debian>
2014-07-05 21:16:10 +0400

    work tests

--- myobj/tests/bootstrap.php
+++ myobj/tests/bootstrap.php
@@ -4,8 +4,13 @@
 $yiit=dirname(__FILE__).'/../../../../../frameworks/yii-1.1.15.022a51/framework/yiit.php';
 $config=dirname(__FILE__).'/../../../config/test.php';
 
-
 require_once($yiit);
+
+Yii::setPathOfAlias('MYOBJ', '/var/www/projects/realty/protected/modules/myobj');
+Yii::import('MYOBJ.models.sys.*');
+Yii::import('MYOBJ.components.cms.behaviors.*');
+
+
 Yii::$enableIncludePath = false;
 require_once(dirname(__FILE__).'/WebTestCase.php');
 
