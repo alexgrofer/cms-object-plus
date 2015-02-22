6c2b553d91a8624812161b2b9f822b5461d33d57
alex <you@example.com>
2015-02-22 20:03:29 +0300

    del pr catalog

--- myobj/appscms/config/admin/UI_catalog.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-	//Независимые интерфейсы, к примеру вывести график продаж или другой интерфейс. В контроллере можно организовать шаблон, представление и т.д.
-	'graphic_sale' => array(
-		'controller' => array('default'=>'admin/dep_store/graphic_sale.php'),
-		'group_read' => 'administrator',
-		'group_write' => 'administrator',
-	),
-);
