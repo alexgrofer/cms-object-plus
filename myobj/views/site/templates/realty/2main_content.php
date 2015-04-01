3d2ab22962d284151fed2eaf6717f9e1f0753e6e
alex <you@example.com>
2015-04-02 02:16:31 +0300

    fix not necessary

--- myobj/views/site/templates/realty/2main_content.php
+++
@@ -1,9 +0,0 @@
-<?php
-//фильтр поиска объектов
-Yii::app()->appcms->handle('filter',1);
-?>
-
-<?php
-//список оъектов
-Yii::app()->appcms->handle('list_obj',2);
-?>
--- myobj/views/site/templates/realty/index_content.php
+++
@@ -1,9 +0,0 @@
-<?php
-//фильтр поиска объявлений
-Yii::app()->appcms->handle('filter',1);
-?>
-
-<?php
-//рекламный блок
-Yii::app()->appcms->handle('ad',2);
-?>
