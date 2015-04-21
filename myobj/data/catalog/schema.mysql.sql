7d22860b0a0a5b460e46df575662bf9926d01dd1
alex <you@example.com>
2015-04-21 14:13:06 +0300

    sql catalog refact

--- myobj/data/catalog/schema.mysql.sql
+++ myobj/data/catalog/schema.mysql.sql
@@ -2,8 +2,7 @@ CREATE TABLE `cmsplus_catalog_category` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`name` varchar(255) NOT NULL,
 	`desc` varchar(255) NOT NULL,
-	`codename` varchar(255) NOT NULL DEFAULT '',
-
+	`codename` varchar(255) NOT NULL,
 	`parent_id` int(11) DEFAULT NULL,
 	PRIMARY KEY (`id`),
 	UNIQUE KEY (`codename`),
@@ -15,7 +14,6 @@ CREATE TABLE `cmsplus_catalog_option` (
 	`name` varchar(255) NOT NULL,
 	`type` tinyint(1) NOT NULL,
 	`conf` varchar(255) NOT NULL DEFAULT '',
-
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -30,7 +28,6 @@ CREATE TABLE `cmsplus_catalog_category_to_option` (
 CREATE TABLE `cmsplus_catalog_option_param` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`val` varchar(255) NOT NULL,
-
 	`id_option` int(11) NOT NULL,
 	PRIMARY KEY (`id`),
 	FOREIGN KEY (`id_option`) REFERENCES `cmsplus_catalog_option` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
