0dd214b8aac2bb61322b57fe5f1d87567b484f23
alex <you@example.com>
2015-08-26 00:55:48 +0300

    validate date js

--- myobj/appscms/src/validators/DateExtendValidator.php
+++ myobj/appscms/src/validators/DateExtendValidator.php
@@ -2,18 +2,25 @@
 
 class DateExtendValidator extends CDateValidator
 {
-
-	/*
-	protected function validateAttribute($object,$attribute) {
-		$value=$object->$attribute;
-		if (CDateTimeParser::parse($value,$this->format)==false) {
-			$this->addError($object, $attribute, $this->message);
-		}
-	}
-	*/
+	/**
+	 * @var string
+	 */
+	public $patternDateJS='/^(\d{2})[-\/](\d{2})[-\/](\d{4})$/';
 
 	public function clientValidateAttribute($object,$attribute) {
-		//return "if(1){messages.push(".CJSON::encode($this->message).");}";
+		return "
+		function isValidDate(date) {
+			var matches = ".$this->patternDateJS.".exec(date);
+			if(matches == null) return false;
+			var d = matches[1];
+			var m = matches[2] - 1;
+			var y = matches[3];
+			var composedDate = new Date(y, m, d);
+			return composedDate.getDate() == d &&
+				composedDate.getMonth() == m &&
+				composedDate.getFullYear() == y;
+		}
+		if(isValidDate(value)==false) {messages.push(".CJSON::encode($this->message).");}";
 	}
 }
 
