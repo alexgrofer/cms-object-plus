d90c854a401af60b508da0462ae018fefe78320d
alex <yo@yo>
2015-10-27 01:49:51 +0300

    refact folder admin assert

--- myobj/views/admin/layouts/column1.php
+++ myobj/views/admin/layouts/column1.php
@@ -1,17 +1,13 @@
 <?php
 $cs = Yii::app()->getClientScript();
 $cs->coreScriptPosition=CClientScript::POS_HEAD; //load into tag
-$assetsFolder=Yii::app()->assetManager->publish(Yii::getPathOfAlias('MYOBJ.assets'));
+$assetsFolder=Yii::app()->assetManager->publish(Yii::getPathOfAlias('MYOBJ.assets.admin'));
 $cs->registerCoreScript('jquery');
 $cs->registerCoreScript('jquery.ui');
 $cs->registerCssFile(
     Yii::app()->clientScript->getCoreScriptUrl().
     '/jui/css/base/jquery-ui.css'
 );
-//custom jquery-ui
-$cs->registerCssFile($assetsFolder.'/jquery-ui/user-jquery-ui.css');
-//$cs->registerCssFile($assetsFolder.'/jquery-ui/user-jquery-ui.js');
-
 $cs->registerScriptFile($assetsFolder.'/bootstrap/js/bootstrap.min.js');
 $cs->registerCssFile($assetsFolder.'/bootstrap/css/bootstrap.min.css');
 
