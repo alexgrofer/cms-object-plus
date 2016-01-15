79d3b6675ec2adf66b4f7134274620746ae88b4b
alex <yo@yo>
2016-01-16 02:09:39 +0300

    refact AbsEnumeration

--- myobj/appscms/src/AbsEnumeration.php
+++ myobj/appscms/src/AbsEnumeration.php
@@ -1,18 +1,21 @@
 <?php
 namespace MYOBJ\appscms\src;
 
-abstract class AbsEnumeration extends CComponent {
-	//const PARAM_1 = 0;
+abstract class AbsEnumeration extends \CComponent {
+	//const ID_PARAM_1 = 0;
 
 	private $_id;
+	private $_name;
 
 	final public static function create($name) {
 		$obj = new static;
 		$obj->_id = $name;
+
+		$fields = $obj->fields();
+		$obj->_name = $fields[$obj->_id];
 		return $obj;
 	}
 
-	abstract static public function fields();
 	/*
 	public function fields() {
 		return array(
@@ -20,8 +23,15 @@ abstract class AbsEnumeration extends CComponent {
 		);
 	}
 	*/
+	public function fields() {
+		return array();
+	}
 
 	final public function getId() {
-		$this->_id;
+		return $this->_id;
+	}
+
+	final public function getName() {
+		return $this->_name;
 	}
 }
