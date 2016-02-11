f65840c76f396fdbf3aeafd5531d99c4979c73e5
alex <yo@yo>
2016-02-11 23:42:58 +0300

    refact file

--- myobj/models/files/AbsStoreFilesModel.php
+++ myobj/models/files/AbsStoreFilesModel.php
@@ -1,1 +1,8 @@
-<?phpabstract class AbsStoreFilesModel extends AbsBaseModel {	public $code_name;	public $folder;	public $size;	public $date_time;	public $parent_id;}
+<?php
+
+abstract class AbsStoreFilesModel extends AbsBaseModel {
+	public $code_name;
+	public $folder;
+	public $size;
+	public $date_time;
+}
--- myobj/models/files/ImageModel.php
+++ myobj/models/files/ImageModel.php
@@ -1,7 +1,5 @@
 <?php
 
 class ImageModel extends AbsStoreFilesModel {
-	public function tableName() {
-		return 'cmsplus_store_file_image';
-	}
+
 }
