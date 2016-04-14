1c82e2d020a6342aa16a5b355c2cf5489ce5a1c3
alex <yo@yo>
2016-04-14 10:51:15 +0400

    refact model files

--- myobj/models/files/AbsStoreFilesModel.php
+++ myobj/models/files/AbsStoreFilesModel.php
@@ -4,5 +4,15 @@ abstract class AbsStoreFilesModel extends AbsBaseModel {
 	public $code_name;
 	public $folder;
 	public $size;
-	public $date_time;
+	public $date_time_create;
+	public $hash;
+
+	public function beforeSave() {
+		if(!parent::beforeSave()) return false;
+
+		if($this->isNewRecord) {
+			$this->date_time_create = new CDbExpression('NOW()');
+		}
+		return true;
+	}
 }
