f119e97bf5dd6d0d9ac97baf40f64244dff3c10a
alex <yo@yo>
2016-07-12 01:38:35 +0400

    admin cookie domain

--- myobj/components/cms/AppCMS.php
+++ myobj/components/cms/AppCMS.php
@@ -32,6 +32,9 @@ class AppCMS extends CComponent {
 			//const
 			SysUtils::importRecursName('MYOBJ.appscms.config.admin','const_*');
 
+			Yii::app()->session->setCookieParams(array('domain'=>$_SERVER['HTTP_HOST']));
+			Yii::app()->user->identityCookie = (array('domain'=>$_SERVER['HTTP_HOST']));
+
 			$this->config = array_merge_recursive($this->config, SysUtils::importRecursName('MYOBJ.appscms.config', 'main_admin.php', true, true));
 		}
 	}
