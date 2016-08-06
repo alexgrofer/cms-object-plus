af3e5449bebc9ff520aac84263e3a77934303a1f
alex <yo@yo>
2016-08-07 01:08:30 +0400

    fix none dform name const

--- myobj/appscms/config/all/const_sys.php
+++ myobj/appscms/config/all/const_sys.php
@@ -3,6 +3,4 @@ defined('DIR_TEMPLATES_SITE') or define('DIR_TEMPLATES_SITE', '/site/templates/'
 defined('DIR_VIEWS_SITE') or define('DIR_VIEWS_SITE', '/site/views/');
 defined('DIR_VIEWS_HANDLES_SITE') or define( 'DIR_VIEWS_HANDLES_SITE', '/site/handles/');
 
-defined('NAME_DF') or define('NAME_DF', 'MYOBJ_appscms_core_base_form_DForm');
-
 defined('NAME_USER_STATE_KEY_IS_ADMIN') or define('NAME_USER_STATE_KEY_IS_ADMIN', 'kdw929usdf34ddf');
