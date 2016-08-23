725e962d2756967a44975dc7fe7b59379e96ad49
alex <yo@yo>
2016-08-23 21:27:35 +0400

    new method SysUtilsArray

--- myobj/appscms/core/base/SysUtilsArray.php
+++ myobj/appscms/core/base/SysUtilsArray.php
@@ -20,4 +20,14 @@ class SysUtilsArray
 		}
 		return $difference;
 	}
+
+	public static function recursive_array_search($needle,$haystack) {
+		foreach($haystack as $key=>$value) {
+			$current_key=$key;
+			if($needle===$value OR (is_array($value) && static::recursive_array_search($needle,$value) !== false)) {
+				return $current_key;
+			}
+		}
+		return false;
+	}
 }
