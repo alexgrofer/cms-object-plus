f39c122bab1b54a2c0257f3ed1da949776825434
alex <yo@yo>
2016-11-13 02:07:31 +0300

    refact ClientDateValidator

--- myobj/appscms/src/validators/ClientDateValidator.php
+++ myobj/appscms/src/validators/ClientDateValidator.php
@@ -1,10 +1,6 @@
 <?php
 
-class DateExtendValidator extends CDateValidator
-{
-	/**
-	 * @var string
-	 */
+class ClientDateValidator extends CValidator {
 	public $patternDateJS='/^(\d{2})[-\/](\d{2})[-\/](\d{4})$/';
 
 	public function clientValidateAttribute($object,$attribute) {
@@ -12,15 +8,26 @@ class DateExtendValidator extends CDateValidator
 		function isValidDate(date) {
 			var matches = ".$this->patternDateJS.".exec(date);
 			if(matches == null) return false;
-			var d = matches[1];
-			var m = matches[2] - 1;
-			var y = matches[3];
+			if(matches[1].length==4) {
+				var y = matches[1];
+				var m = matches[2];
+				var d = matches[3];
+			}
+			else {
+				var d = matches[1];
+				var m = matches[2];
+				var y = matches[3];
+			}
 			var composedDate = new Date(y, m, d);
 			return composedDate.getDate() == d &&
 				composedDate.getMonth() == m &&
 				composedDate.getFullYear() == y;
 		}
 		if(isValidDate(value)==false) {messages.push(".CJSON::encode($this->message).");}";
+	}
+
+	protected function validateAttribute($object,$attribute) {
+		return true;
 	}
 }
 
